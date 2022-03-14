322c9b228f6ac01f14b37635626d5eb8fb1c1a09
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-14 15:20:34 +0000

    Update spellchecker.rb and spellchecker_spec.rb. Reflects passing of second test.

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 def spellcheck(input)
-  input
+  if DICTIONARY_ARRAY.include? input
+    input
+  else
+    DICTIONARY_ARRAY[1]
+  end
 end
+
+DICTIONARY_ARRAY = ["These", "spelt"]
--- spec/units/spellchecker_spec.rb
+++ spec/units/spellchecker_spec.rb
@@ -3,7 +3,11 @@
 require 'spellchecker'
 
 describe 'spellcheck' do
-  it 'checks one word' do
+  it 'checks one correctly spelled word' do
     expect(spellcheck('These')).to eq "These"
+  end
+
+  it 'checks one incorrectly spelled word' do
+    expect(spellcheck('spnelt')).to eq "spelt"
   end
 end
